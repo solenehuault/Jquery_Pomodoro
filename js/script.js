f3530eed3023b02129eda47c998c6cab1d9587db
solenehuault <solene.huault@gmail.com>
2016-07-25 09:52:06 +0200

    fonction appelé via html cassé réparé

--- js/script.js
+++ js/script.js
@@ -4,21 +4,9 @@
  * * * * * * * * * * * * * * *
  * * * * * * * * * * * * * * */
 
-
-$(document).ready(function() {
-
 var seconde = 1;
 var minute = 0;
-var time_is_running = false; 
-
-$("#play").click( chrono(time_is_running, seconde, minute) );
-$("#pause").click( chrono_pause() );
-
-$("#ongoing_done").click( taskdone() );
-
-});
-
-
+var time_is_running = false;
 
 function taskdone() {
 	if ($("#task").val() != "") {
@@ -28,24 +16,22 @@ function taskdone() {
 };
 
 function chrono(time_is_running, seconde, minute) {
-	if (!time_is_running) {
-		time_is_running = true;
-		var timer = setInterval(function() {
-			if (seconde == 59) {
-				seconde = 0;
-				minute++;
-				$(".time").text(minute + ":" + seconde);
-			}
-			if (seconde < 10) {
-				$(".time").text(minute + ":0" + seconde);
-				seconde++;
-			}
-			else {
-				$(".time").text(minute + ":" + seconde);
-				seconde++;
-			}
-		}, 1000);
-	}
+	$("#play").attr("disabled", true);
+	var timer = setInterval(function() {
+		if (seconde == 59) {
+			seconde = 0;
+			minute++;
+			$(".time").text(minute + ":" + seconde);
+		}
+		if (seconde < 10) {
+			$(".time").text(minute + ":0" + seconde);
+			seconde++;
+		}
+		else {
+			$(".time").text(minute + ":" + seconde);
+			seconde++;
+		}
+	}, 1000);
 };
 
 function startbreak() {
