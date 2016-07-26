ed6d3f54385da0595d1351365b636a8e0e5892f3
solenehuault <solene.huault@gmail.com>
2016-07-26 10:52:26 +0200

    timer réparé

--- js/script.js
+++ js/script.js
@@ -3,30 +3,26 @@
  * * YOUR CODE GOES HERE ! * *
  * * * * * * * * * * * * * * *
  * * * * * * * * * * * * * * */
-
-var seconde = 6;
+var seconde = 10;
 var minute = 0;
-var timer;
+var timer; //storage of setInterval
 var nbr_pause = 0;
 var having_a_break = false;
 
-//function chrono
+
 function chrono() {
-	timer = setInterval(function() {
-		console.log("chrono: " + minute, seconde);
+	timer = setInterval(function () {
 		if (seconde == 0) {
 			seconde = 59;
 			minute--;
-			$(".time").text(minute + ":" + seconde);
-		}
-		if (seconde < 10) {
-			seconde--;
-			$(".time").text(minute + ":0" + seconde);
 		}
 		else {
 			seconde--;
-			$(".time").text(minute + ":" + seconde);
 		}
+		
+		var temps = seconde < 10 ? minute + ":0" + seconde : minute + ":" + seconde;
+		$(".time").text(temps);
+		
 		if (seconde == 0 && minute == 0 && !having_a_break) {
 			having_a_break = true;
 			startbreak();
@@ -36,13 +32,12 @@ function chrono() {
 	$("#play").attr("disabled", true);
 	$("#pause").attr("disabled", false);
 	$("#stop").attr("disabled", false);
-	console.log("chrono-sortie: " + minute, seconde);
 };
 
-function chrono_pause (timer) {
-console.log("avt pause:" + minute, seconde);
+
+function chrono_pause(timer) {
 	clearInterval(timer);
-	console.log("pause:" + minute, seconde);
+	
 	$("#play").attr("disabled", false);
 	$("#pause").attr("disabled", true);
 	$("#stop").attr("disabled", false);
@@ -52,29 +47,29 @@ function chrono_stop(timer) {
 	clearInterval(timer);
 	seconde = 0;
 	minute = 25;
-	console.log("stop:" + minute, seconde);
-	$(".time").text("0:00");
+	
+	$(".time").text("25:00");
 	$("#play").attr("disabled", false);
 	$("#pause").attr("disabled", false);
 	$("#stop").attr("disabled", true);
-	
 };
-
 
 
 //function task/break
 function startbreak() {
-	
-	if (nbr_pause == 3) {minute = 20;}
-	else if (having_a_break){
+	if (nbr_pause == 3) {
 		minute = 0;
-		seconde = 4;
+		seconde = 20;
+	}
+	else if (having_a_break) {
+		minute = 0;
+		seconde = 5;
 		nbr_pause++;
 		having_a_break = false;
 	}
 	else {
 		minute = 0;
-		seconde = 6;
+		seconde = 10;
 	}
 };
 
