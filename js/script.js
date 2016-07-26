c5b63bcb1131c4b08789f8e6179d4360e6c0d416
solenehuault <solene.huault@gmail.com>
2016-07-26 12:21:39 +0200

    gerage des pauses (mini break check)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
-/* * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * *
- * * YOUR CODE GOES HERE ! * *
- * * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * */
 var seconde = 10;
 var minute = 0;
 var timer; //storage of setInterval
 var nbr_pause = 0;
-var having_a_break = false;
-
+var having_a_break = true;
 
 function chrono() {
 	timer = setInterval(function () {
@@ -19,25 +13,19 @@ function chrono() {
 		else {
 			seconde--;
 		}
-		
 		var temps = seconde < 10 ? minute + ":0" + seconde : minute + ":" + seconde;
 		$(".time").text(temps);
-		
-		if (seconde == 0 && minute == 0 && !having_a_break) {
-			having_a_break = true;
+		if (seconde == 0 && minute == 0) {
 			startbreak();
 		}
 	}, 1000);
-	
 	$("#play").attr("disabled", true);
 	$("#pause").attr("disabled", false);
 	$("#stop").attr("disabled", false);
 };
 
-
 function chrono_pause(timer) {
 	clearInterval(timer);
-	
 	$("#play").attr("disabled", false);
 	$("#pause").attr("disabled", true);
 	$("#stop").attr("disabled", false);
@@ -47,32 +35,32 @@ function chrono_stop(timer) {
 	clearInterval(timer);
 	seconde = 0;
 	minute = 25;
-	
 	$(".time").text("25:00");
 	$("#play").attr("disabled", false);
 	$("#pause").attr("disabled", false);
 	$("#stop").attr("disabled", true);
 };
-
-
 //function task/break
 function startbreak() {
-	if (nbr_pause == 3) {
-		minute = 0;
-		seconde = 20;
-	}
-	else if (having_a_break) {
+	if (having_a_break) {
+		if (nbr_pause == 3) {
+			minute = 0;
+			seconde = 20;
+			having_a_break = false;
+		}
 		minute = 0;
 		seconde = 5;
 		nbr_pause++;
+		console.log("nbr_pause: " + nbr_pause);
 		having_a_break = false;
 	}
 	else {
 		minute = 0;
 		seconde = 10;
+		having_a_break = true;
 	}
 };
-
+//display the ongoing task in a the done table when the done button is clicked
 function taskdone() {
 	if ($("#task").val() != "") {
 		$("#done").prepend("<li class='list-group-item'><s>" + $("#task").val() + "</s></li>");
